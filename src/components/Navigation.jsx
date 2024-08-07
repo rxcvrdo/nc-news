d57ae2d502176fbc42777e8c6d780f4f7eb8d85e
rxcvrdo <rmurdock177@hotmail.co.uk>
2024-08-07 16:12:24 +0100

    with added url

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,23 @@
 import { Link} from "react-router-dom"
 import PostArticle from "./PostArticle"
 import Users from "./Users"
+import Avatar from "./Avatar"
+import { UserContext } from "../contexts/User"
+import { useContext } from "react"
+
 function Navigation (){
+    const {user} = useContext(UserContext)
     return (
         <>
         <nav>
             <Link to= "/post-article" element = {<PostArticle/>}>post </Link>
             <Link to= "/users" element={<Users/>}>users </Link>
+            {user && (
+                <Avatar 
+                avatarUrl={user.avatar_url}
+                username={user.username}
+                name={user.name}/>
+            )}
         </nav>
         </>
     )
