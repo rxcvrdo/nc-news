26e5a392470f04e14ad6d60392692014baa4cc43
rxcvrdo <rmurdock177@hotmail.co.uk>
2024-09-11 11:07:19 +0100

    new homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,43 @@
-import { useContext} from "react"
-import { UserContext } from "../contexts/User"
-/* eslint-disable react/prop-types */
+import { useContext, useState, useEffect } from "react";
+import { UserContext } from "../contexts/User";
+import { getUsers } from "../api"; 
 
-useContext
+function Home() {
+  const { user, setUser } = useContext(UserContext);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-function Home(){
-const{user} = useContext(UserContext)
+  useEffect(() => {
+    getUsers().then((fetchedUsers) => {
+      setUsers(fetchedUsers);
+      setLoading(false);
+    });
+  }, []);
 
-    return(
-        <h1>Welcome {user.username}</h1>
-    )
+  function handleUserChange(event) {
+    const selectedUsername = event.target.value;
+    const selectedUser = users.find((u) => u.username === selectedUsername);
+    setUser(selectedUser);
+  }
+
+  if (loading) return <p>Loading users...</p>;
+
+  return (
+    <div className="home-container">
+      <h1>Welcome, {user.username}</h1>
+      <p>Select a user to switch accounts:</p>
+
+      <div className="user-select-container">
+        <select value={user.username} onChange={handleUserChange}>
+          {users.map((u) => (
+            <option key={u.username} value={u.username}>
+              {u.name} ({u.username})
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
